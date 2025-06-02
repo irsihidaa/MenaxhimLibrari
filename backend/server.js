f52a2935b64c0bf7be6d9e916e124ba9df6a92d8
irsihidaa <ihida@uet.edu.al>
2025-06-02 21:46:43 +0000

    login dhe register

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,24 @@
-const express = require('express')
+const express = require('express');
 const { errorHandler } = require('./middleware/errorMiddleware');
-const dotenv = require('dotenv').config()
+const dotenv = require('dotenv').config();
 const connectDB = require('./connect/database');
 const port = process.env.PORT || 8000;
-const app = express();
-
+const Cors = require('cors');
 
 connectDB();
+const app = express();
+
+app.use(Cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/books', require('./routes/bookRoutes'));
+app.use('/api/tasks', require('./routes/bookRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
-app.use(errorHandler);
 
-    
+
+
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
